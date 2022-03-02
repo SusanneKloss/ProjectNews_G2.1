49b0d24c4c642ed077bc5899796a8d7ccbb21bdc
magdalenkeller <magdalena.keller@stud.fh-campuswien.ac.at>
2022-03-02 10:57:11 +0100

    added Article Class

---
+++ src/main/java/Article.java
@@ -0,0 +1,25 @@
+public class Article {
+
+    private String author;
+    private String title;
+
+    public Article(String author, String title){
+
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
