a62b06a61dfda29526fa821e42716fb7a86857d7
lenagross <lena.gross@stud.fh-campuswien.ac.at>
2022-03-02 11:24:57 +0100

    app controller: added mocklist

--- src/main/java/AppController.java
+++ src/main/java/AppController.java
@@ -23,6 +23,16 @@ public class AppController {
     protected List<Article> filterList(String query, List<Article> articles){
     }
     private List<Article> generateMockList(){
+        Article one =  new Article("Karl Marx", "Das Kapital");
+        articles.add(one);
+        Article two = new Article("Peter Molyneux", "Why i am a god");
+        articles.add(two);
+        Article three = new Article("Angela Merkel", "Wie ich die Raute erfand");
+        articles.add(three);
+        Article four = new Article("Donald Trump", "My orange hair");
+        articles.add(four);
+        Article five = new Article("Carl Barks", "Dagobert - Sein Leben, Seine Milliarden");
+        articles.add(five);
     }
 
 }
