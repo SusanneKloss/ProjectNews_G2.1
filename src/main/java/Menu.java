1f9f5e1f10647b15a07045227d9ca7f38a186a5d
Berserk12 <hamza_k@live.at>
2022-03-02 11:08:53 +0100

    Class Menu created.

---
+++ src/main/java/Menu.java
@@ -0,0 +1,39 @@
+public class Menu {
+
+    private AppController controller;
+    private static String INVALID_INPUT_MESSAGE;
+    private static String EXIT_MESSAGE;
+
+
+
+
+    public static void start(){
+
+
+    }
+    private void handleinput(String input){
+
+    }
+    private void getArticleCount(AppController ctrl){
+
+    }
+    private void getTopHeadlinesAustria(AppController ctrl){
+
+    }
+    private void getAllNewsBitcoin(AppController ctrl){
+
+    }
+    private static void printExitMessages(){
+
+    }
+    private static void printInvalidInputMessages(){
+
+    }
+    private static void printMenu(){
+
+    }
+
+
+
+
+}
