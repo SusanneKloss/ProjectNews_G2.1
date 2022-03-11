d052beff11715b6a28d708c1459e91bf537ecec8
susikloss <susanne.kloss@stud.fh-campuswien.ac.st>
2022-03-11 16:29:39 +0100

    getMocklist() added AppControllerTest: @BeforeAll, @BeforeEach added

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -1,4 +1,22 @@
 package at.ac.fhcampuswien;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
 public class AppControllerTest {
+    private static AppController ac;
+    ArrayList<Article> actual;
+    Article one =  new Article("Karl Marx", "Das Kapital");
+
+    @BeforeAll
+    static void init(){
+        ac = new AppController();
+    }
+
+    @BeforeEach
+    void setup(){
+        actual = new ArrayList<>();
+    }
 }
