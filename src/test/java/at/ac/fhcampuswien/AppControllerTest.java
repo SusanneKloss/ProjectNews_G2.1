8539c0c3390f33a3c40c372655e78ebdf5debc31
susikloss <susanne.kloss@stud.fh-campuswien.ac.st>
2022-03-11 16:33:34 +0100

    testSetArticles_Scenario1 ("Article List not empty") added

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -2,8 +2,12 @@ package at.ac.fhcampuswien;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppControllerTest {
     private static AppController ac;
@@ -18,5 +22,16 @@ public class AppControllerTest {
     @BeforeEach
     void setup(){
         actual = new ArrayList<>();
+    }
+
+    @Test
+    @DisplayName("Article List not empty")
+    public void testSetArticles_Scenario1() {
+
+        //action
+        actual = AppController.getMockList();
+
+        //assertion
+        assertTrue(actual.size() > 0);
     }
 }
