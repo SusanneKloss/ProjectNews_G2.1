af1030fcb1b1aa0fe5ff0b9143667d4eb9712380
Berserk12 <hamza_k@live.at>
2022-03-17 11:08:04 +0100

    Added FilterList Test.

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -73,5 +75,27 @@ public class AppControllerTest {
 
         //assertion
         assertEquals(expectedCount, actualCount);
+    }
+    @Test
+    @DisplayName("Correct Number of Strings(Titles)")
+    public void testFilterList_Scenario1(){
+
+        Article one =  new Article("Karl Marx", "Das Kapital");
+        actual.add(one);
+        Article two = new Article("Peter Molyneux", "Why i am a god");
+        actual.add(two);
+        Article three = new Article("Angela Merkel", "Wie ich die Raute erfand");
+        actual.add(three);
+        Article four =  new Article("Karl Marx", "das Kapital");
+        actual.add(four);
+        actual = ac.filterList("Das",actual);
+
+        assertEquals(2,actual.size());
+
+       /* assertTrue(actual.size() > 0);
+        assertTrue(actual.contains(one.getTitle()));
+        assertEquals(actual.contains(one.getTitle()),actual.remove(one.getAuthor()));*/
+
+
     }
 }
