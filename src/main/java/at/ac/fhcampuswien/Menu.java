7409772b22d0feeaf4208ae579af3887549f24f3
lenagross <lena.gross@stud.fh-campuswien.ac.at>
2022-03-28 21:27:47 +0200

    - added over effects - updated pom.xml (added javafx.plugin) - updated javafx

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -1,8 +1,12 @@
 package at.ac.fhcampuswien;
 
 import at.ac.fhcampuswien.AppController;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -14,9 +18,10 @@ public class Menu {
     private StringBuilder textOutput;
     private static String INVALID_INPUT_MESSAGE;
     private static String EXIT_MESSAGE;
+    //private SFX soundInMenu = new SFX();
 
     public javafx.scene.control.Button austriaButton, bitcoinButton, countButton, quitButton;
-    public Text textField;
+    public Text textField, austriaLetters, bitcoinLetters, countLetters, quitLetters;
 
 
     public static void start(){
@@ -27,7 +32,9 @@ public class Menu {
     }
     public void getArticleCount(ActionEvent actionEvent){
         textField.setText("Number of articles: " + String.valueOf(controller.getArticleCount()));
+        //soundInMenu.playClick();
     }
+
     public void getTopHeadlinesAustria(ActionEvent actionEvent){
         textOutput = new StringBuilder();
         outputList = controller.getTopHeadlinesAustria();
@@ -36,7 +43,9 @@ public class Menu {
             textOutput.append(art.toString());
         }
         textField.setText(textOutput.toString());
+        //soundInMenu.playClick();
     }
+
     public void getAllNewsBitcoin(ActionEvent actionEvent){
         textOutput = new StringBuilder();
         outputList = controller.getAllNewsBitcoin();
@@ -45,14 +54,58 @@ public class Menu {
             textOutput.append(art.toString());
         }
         textField.setText(textOutput.toString());
+        //soundInMenu.playClick();
     }
+
     public void printExitMessages(ActionEvent actionEvent){
-        System.exit(0);
+        textField.setText("Bye bye!");
+        //soundInMenu.playClick();
+        PauseTransition pauseEnd = new PauseTransition(Duration.seconds(2));
+        pauseEnd.setOnFinished(event1 -> {
+            System.exit(0);
+        });
+        pauseEnd.play();
     }
     private static void printInvalidInputMessages(){
 
     }
     private static void printMenu(){
 
+    }
+
+    public void austriaEntered(MouseEvent mouseEvent) {
+        austriaLetters.setFill(Color.web("#080808"));
+        //soundInMenu.playHover();
+    }
+
+    public void austriaExited(MouseEvent mouseEvent) {
+        austriaLetters.setFill(Color.web("#858585"));
+    }
+
+    public void bitcoinEntered(MouseEvent mouseEvent) {
+        bitcoinLetters.setFill(Color.web("#080808"));
+        //soundInMenu.playHover();
+    }
+
+    public void bitcoinExited(MouseEvent mouseEvent) {
+        bitcoinLetters.setFill(Color.web("#858585"));
+    }
+
+    public void countEntered(MouseEvent mouseEvent) {
+        countLetters.setFill(Color.web("#080808"));
+        //soundInMenu.playHover();
+    }
+
+    public void countExited(MouseEvent mouseEvent) {
+        countLetters.setFill(Color.web("#858585"));
+    }
+
+    public void quitEntered(MouseEvent mouseEvent) {
+        quitLetters.setFill(Color.web("#080808"));
+        //soundInMenu.playHover();
+    }
+
+    public void quitExited(MouseEvent mouseEvent) {
+        quitLetters.setFill(Color.web("#858585"));
     }
 }
---
+++ src/main/java/at/ac/fhcampuswien/SFX.java
@@ -0,0 +1,27 @@
+package at.ac.fhcampuswien;
+
+import javafx.scene.media.AudioClip;
+
+import java.io.File;
+
+public class SFX {
+
+    /**
+    String clickPath = "src/main/resources/mixkit-plastic-bubble-click-1124.wav";
+    AudioClip click = new AudioClip(new File(clickPath).toURI().toString());
+
+    String hoverPath = "src/main/resources/beep-rollover-13-sound-effect-49884034.mp3";
+    AudioClip hover = new AudioClip((new File(hoverPath).toURI().toString()));
+
+    public void playClick(){
+        click.setVolume(0.4);
+        click.play();
+    }
+
+    public void playHover(){
+        hover.setVolume(0.4);
+        hover.play();
+    }
+
+     **/
+}
