886f0501fa1fe5fd7b4779d67381a33e99b1de2e
magdalenkeller <magdalena.keller@stud.fh-campuswien.ac.at>
2022-04-28 11:22:36 +0200

    NewsSource class added

--- src/main/java/at/ac/fhcampuswien/NewsSource.java
+++ src/main/java/at/ac/fhcampuswien/NewsSource.java
@@ -1,4 +1,41 @@
 package at.ac.fhcampuswien;
 
+import java.util.ArrayList;
+
 public class NewsSource {
+
+        private String status;
+        private int totalResults;
+        private ArrayList<Article> articles;
+
+        private String code;
+        private String message;
+
+        public NewsResponse(String status, int totalResults, ArrayList<Article> articles){
+            this.status = status;
+            this.totalResults = totalResults;
+            this.articles = articles;
+        }
+        //Response object for errors - https://newsapi.org/docs/errors
+        public NewsResponse(String status, String code, String message){
+            this.status = status;
+            this.code = code;
+            this.message = message;
+        }
+
+        public String getStatus(){
+            return status;
+        }
+
+        public int getTotalResults(){
+            return totalResults;
+        }
+
+        public ArrayList<Article> getArticles(){
+            return articles;
+        }
+
+        public String getMessage(){
+            return message;
+        }
 }
