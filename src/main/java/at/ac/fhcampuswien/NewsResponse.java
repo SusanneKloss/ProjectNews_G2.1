8818b5faf6d8959d76695bac16cfd039db3a30f8
magdalenkeller <magdalena.keller@stud.fh-campuswien.ac.at>
2022-04-28 09:12:38 +0200

    article class

---
+++ src/main/java/at/ac/fhcampuswien/NewsResponse.java
@@ -0,0 +1,9 @@
+package at.ac.fhcampuswien;
+
+public class NewsResponse {
+
+    private static String API_KEY = "078504f64e1c4b6996e5a1b8e25798f7";
+
+    //public String buildUrl(String query, Enum...e){}
+
+}
